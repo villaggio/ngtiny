771ed582ea61af43fa0e05de54edce7511c785fc
Nicolò Crisafulli <nicolo.crisafulli@villaggio.org>
2016-10-22 16:19:43 +0200

    Aggiunta configurazione

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,28 +1,34 @@
 var app = angular.module('ngtiny', ['ngRoute']);
-app.conf=function ($routeProvider) { 
-  $routeProvider 
-    .when('/', { 
-      //controller: 'HomeController', 
-      templateUrl: 'views/home.html' 
+app.conf=function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      //controller: 'HomeController',
+      templateUrl: 'views/home.html'
     })
-    .when('/students', { 
-      controller: 'StudentsController', 
-      templateUrl: 'views/students.html' 
-    }) 
-    .when('/lessons', { 
-      controller: 'LessonsController', 
-      templateUrl: 'views/lessons.html' 
-    }) 
-    .when('/agenda', { 
-      controller: 'AgendaController', 
-      templateUrl: 'views/agenda.html' 
-    }) 
-    .when('/credits', { 
-      //controller: 'CreditsController', 
-      templateUrl: 'views/credits.html' 
-    }) 
-    .otherwise({ 
-      redirectTo: '/' 
-    }); 
+    .when('/students', {
+      controller: 'StudentsController',
+      templateUrl: 'views/students.html'
+    })
+    .when('/lessons', {
+      controller: 'LessonsController',
+      templateUrl: 'views/lessons.html'
+    })
+    .when('/agenda', {
+      controller: 'AgendaController',
+      templateUrl: 'views/agenda.html'
+    })
+    .when('/credits', {
+      //controller: 'CreditsController',
+      templateUrl: 'views/credits.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
 }
 app.config(app.conf);
+app.constant("swInfo", {
+    "version": "1.0.1"
+});
+app.constant("URL", {
+    "REST": "server"
+});
