9111afdd19e1a55aa8bd6fa3cece73105c981ec1
Fabio Fazio <fabio.fazio@villaggio.org>
2016-10-26 19:08:16 +0200

    Servizio di login/logout con sessione in sessionstorage usata in home

--- public_html/js/controllers/appController.js
+++ public_html/js/controllers/appController.js
@@ -1,20 +1,6 @@
 app.controller('AppController', ['swInfo', function( swInfo ) {
     var vm = this;
-    vm.nav = [
-       {
-        url:"#/",
-        title:"Home"
-       },{
-        url:"#/students",
-        title:"Anagrafica"
-       },{
-        url:"#/agenda",
-        title:"Calendario"
-       },{
-        url:"#/credits",
-        title:"Crediti"
-       }
-    ];
+    
     vm.ver = swInfo.version;
     vm.today = new Date();
 }]);
--- public_html/js/directives/appNavDirective.js
+++ public_html/js/directives/appNavDirective.js
@@ -1,10 +1,40 @@
 app.directive('appNav', function() { 
   return { 
     restrict: 'E', 
-    scope: { 
-      items: '=' 
-    }, 
     templateUrl: 'js/directives/appNav.html' 
   }; 
 });
+
+app.controller('appNavController', [ '$scope', 'SessionService', function($scope, SessionService) { 
+  var vm = this;
+  vm.pages = [];
+  vm.session = null;
+  
+  vm.loadSession = function(){
+      vm.pages.length = 0;
+      vm.session = SessionService.get();
+      if(vm.session){
+        for(i = 0; i < vm.session.pages.length ; i++){
+            vm.pages.push(vm.session.pages[i]);
+        }
+    } 
+  }
+  
+  vm.init = function(){
+      vm.loadSession();
+  }
+  
+  vm.signout = function(){
+        SessionService.signout(vm.loadSession);
+  }
+  
+  vm.signin = function(valid){
+    if(valid){
+        SessionService.signin({data: vm.user}, vm.loadSession);
+    }else
+        alert("Autenticazione errata");
+  }
+  
+  vm.init();
+}]);
 
